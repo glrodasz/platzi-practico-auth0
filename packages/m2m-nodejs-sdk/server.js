1779e47c6e6d7bb24f732f82d898ff26c19d0d68
Guillermo Rodas <me@guillermorodas.com>
2023-02-15 22:44:05 +0100

    Machine to Machine Express

--- packages/m2m-nodejs-sdk/server.js
+++ packages/m2m-nodejs-sdk/server.js
@@ -1,45 +1,52 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
+const cors = require("cors");
+require("dotenv").config();
+
+const { auth, requiredScopes } = require("express-oauth2-jwt-bearer");
 
 if (!process.env.ISSUER_BASE_URL || !process.env.AUDIENCE) {
-  throw 'Make sure you have ISSUER_BASE_URL, and AUDIENCE in your .env file';
+  throw "Make sure you have ISSUER_BASE_URL, and AUDIENCE in your .env file";
 }
 
-const corsOptions =  {
-  origin: 'http://localhost:3000'
+const corsOptions = {
+  origin: "http://localhost:3000",
 };
 
 app.use(cors(corsOptions));
 
-// TODO: Add the auth middleware
-const checkJwt = null
+const checkJwt = auth();
 
-app.get('/api/public', function(req, res) {
+app.get("/api/public", function (req, res) {
   res.json({
-    message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+    message:
+      "Hello from a public endpoint! You don't need to be authenticated to see this.",
   });
 });
 
-// TODO: Add the checkJwt middleware
-app.get('/api/private', function(req, res) {
+app.get("/api/private", checkJwt, function (req, res) {
   res.json({
-    message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+    message:
+      "Hello from a private endpoint! You need to be authenticated to see this.",
   });
 });
 
-// TODO: Add the requiredScopes middleware
-app.get('/api/private-scoped', checkJwt, function(req, res) {
-  res.json({
-    message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
-  });
-});
+app.get(
+  "/api/private-scoped",
+  checkJwt,
+  requiredScopes("read:messages"),
+  function (req, res) {
+    res.json({
+      message:
+        "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.",
+    });
+  }
+);
 
-app.use(function(err, req, res, next){
+app.use(function (err, req, res, next) {
   console.error(err.stack);
   return res.set(err.headers).status(err.status).json({ message: err.message });
 });
 
 app.listen(3010);
-console.log('Listening on http://localhost:3010');
+console.log("Listening on http://localhost:3010");
